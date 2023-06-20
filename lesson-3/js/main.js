e06705d62d6e5219f772f2acc34dd3691400d3ac
virkann <virkan90@gmail.com>
2023-06-20 15:45:44 +0300

    new text readme

--- lesson-3/js/main.js
+++ lesson-3/js/main.js
@@ -4,8 +4,12 @@
 // console.log(`sum=${(sum)}`)
   
   
-function sum(a, b) {
-    return a + b;
-  }
+// function sum(a, b) {
+//     return a + b;
+//   }
   
-  console.log(`1 + 2 = ${sum(1, 2)}`);// 1+2=3
+//   console.log(`1 + 2 = ${sum(1, 2)}`);// 1+2=3
+
+var x = new String('Привет, мир');
+
+console.log(x.toString()); // Отобразит 'Привет, мир'
