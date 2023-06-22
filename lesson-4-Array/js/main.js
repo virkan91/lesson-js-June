2dcebaf908a48c8758c8d37893a7126edcf82856
virkann <virkan90@gmail.com>
2023-06-23 02:50:58 +0300

    add task

---
+++ lesson-4-Array/js/main.js
@@ -0,0 +1,139 @@
+
+
+/* task -1 
+Создайте функцию, которая принимает массив, содержащий
+только числа, и возвращает первый элемент.*/
+
+// function getFirstValue(arr){
+//    return arr.at()
+   
+// }
+// console.log(getFirstValue([1,2,3]))
+// console.log(getFirstValue([80,5,100]))
+// console.log(getFirstValue([-500,0,50]))
+
+
+/* Task - 2
+Учитывая два аргумента, верните массив, содержащий эти два
+аргумента.*/
+
+    // function makePair(num1, num2) {
+    // 	let arr = ([num1,num2])
+    // 	return arr
+    // }
+    // console.log(makePair(1,2))
+    // console.log(makePair(51, 21))
+
+
+/* Task -3 
+Напишите функцию для обратного преобразования массива. */
+
+// function reverse(arr){
+//     return arr.reverse()
+
+// }
+// console.log(reverse([1, 2, 3, 4]))
+
+
+/* Task -4 
+Помогите исправить все ошибки в функции увеличения
+элементов! Предполагается добавить 1 к каждому элементу в
+массиве!*/
+
+// function incrementItems(arr) {
+// 	for (let i = 0; i < arr.length; i++) {
+// 		arr[i] += 1;
+// 	}
+// 	return arr;
+// }
+// console.log(incrementItems([0, 1, 2, 3]))
+// console.log(incrementItems([-1, -2, -3, -4]))
+
+
+
+/* Task - 5 
+Создайте функцию, которая принимает массив и возвращает
+последний элемент в массиве.*/
+
+    // function getLastItem(arr){
+    //     return arr.at(-1)
+    // }
+    // console.log(getLastItem([1,2,3]))
+    // console.log(getLastItem(["cat", "dog", "duck"]))
+    // console.log(getLastItem([true, false, true]))
+
+
+/* task - 6 
+Создайте функцию, которая принимает массив цифр или букв
+и возвращает строку.*/
+
+    // function arrayToString(arr){
+    //     return arr.join("")
+    // }
+    // console.log(arrayToString([1, 2, 3, 4, 5, 6]))
+
+
+/* Task -7
+ Напишите функцию, чтобы проверить, содержит ли массив
+определенное число. */
+
+// function check(arr){
+//     return arr.includes()
+// }
+// console.log(check([1, 2, 3, 4, 5], 3))
+// console.log(check([1, 1, 2, 1, 1], 3))
+// console.log(check([5, 5, 5, 6], 5))
+
+
+/* Task - 8 
+Создайте функцию, которая принимает массив и строку в
+качестве аргументов и возвращает индекс строки.*/
+
+// function findIndex(arr,str){
+//    return arr.indexOf(str)
+// }
+// console.log(findIndex(["hi", "edabit", "fgh", "abc"], "fgh"))
+// console.log(findIndex(["Red", "blue", "Blue", "Green"], "blue"))
+
+
+/* Task - 9 
+Создайте функцию для объединения двух целочисленных
+массивов.*/
+
+// function concat(arr1,arr2){
+//    return arr1.concat(arr2)
+// }
+// console.log(concat([1, 3, 5], [2, 6, 8]))
+// console.log(concat([7, 8], [10, 9, 1, 1, 2]))
+
+
+/* Task - 10 
+Создайте функцию, которая принимает в качестве параметра
+массив "строковых" чисел и возвращает массив чисел.*/
+
+// function toNumberArray(arr) {
+// 	return arr.map(Number)
+// }
+// console.log(toNumberArray(["9.4", "4.2"]))
+
+
+/* Task - 11
+Создайте функцию, которая принимает массив целых чисел и
+строк. Преобразуйте целые числа в строки и верните новый
+массив. */
+
+// function parseArray(arr){
+
+// }
+
+/* Task -12 
+Исправьте код на вкладке "Код", чтобы пройти этот тест
+(только синтаксические ошибки). Посмотрите на приведенные
+ниже примеры, чтобы получить представление о том, что должна
+делать функция.*/
+
+    // function sumArray(arr){
+    //   return arr.reduce((a,b) => a+b)
+    // }
+    // console.log(sumArray([1, 2, 3, 4, 5]))
+    // console.log(sumArray([-1, 0, 1]))
