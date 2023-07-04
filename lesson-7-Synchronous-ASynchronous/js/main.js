866becd023c5f7f7e09f6ee4d54ef408f7567ff5
virkann <virkan90@gmail.com>
2023-07-05 02:52:44 +0300

    add img

--- lesson-7-Synchronous-ASynchronous/js/main.js
+++ lesson-7-Synchronous-ASynchronous/js/main.js
@@ -1,4 +1,20 @@
 
 
 
-let box =document.querySelector('box')
+// SYNCHRONOUS code 
+
+let box =document.querySelector('box') // 1 step
+
+console.log("one")  // 2 step
+console.log("two") // 3 step
+console.log("three") // 4 step
+
+
+
+
+
+
+
+
+
+
