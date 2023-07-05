4da7ce73a7a4327ca04436128a8fca8c0b4d69f9
virkann <virkan90@gmail.com>
2023-07-05 03:00:17 +0300

    second commit

--- lesson-7-Synchronous-ASynchronous/js/main.js
+++ lesson-7-Synchronous-ASynchronous/js/main.js
@@ -3,11 +3,19 @@
 
 // SYNCHRONOUS code 
 
-let box =document.querySelector('box') // 1 step
+// let box =document.querySelector('box') // 1 step
 
-console.log("one")  // 2 step
-console.log("two") // 3 step
-console.log("three") // 4 step
+// console.log("one")  // 2 step
+// console.log("two") // 3 step
+// console.log("three") // 4 step
+
+
+
+
+   // ansy  code
+
+
+
 
 
 
