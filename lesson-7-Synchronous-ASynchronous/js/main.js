17c3309df1468215de3f6fca855cb30c069d0393
virkann <virkan90@gmail.com>
2023-07-05 03:57:24 +0300

    add text readme

--- lesson-7-Synchronous-ASynchronous/js/main.js
+++ lesson-7-Synchronous-ASynchronous/js/main.js
@@ -5,15 +5,30 @@
 
 // let box =document.querySelector('box') // 1 step
 
-// console.log("one")  // 2 step
-// console.log("two") // 3 step
-// console.log("three") // 4 step
+
+
+console.log("one")  // SYNCHRONOUS code  
+
+
+setTimeout(()=>{ 
+    console.log('salom')   // ASYNCHRONOUS code 
+  }, 2000)
+
+
+  
+console.log("two") // SYNCHRONOUS code 
+
 
 
 
 
    // ansy  code
 
+// setTimeout(()=>{   // setTimeout - 2 параматр мегира (1.callbacks(функция) 2-юмш Ожидание минисикунд мегира) 
+//   console.log('salom')
+// }, 2000)
+
+// setTimeout - мтоды dom-ай
 
 
 
